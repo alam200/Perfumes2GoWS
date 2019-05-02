aa7807589517995d2be92f85aef55830c649c8e4
alidavid0418 <robbie626827@gmail.com>
2019-05-02 13:06:01 +0900

    adjust product.route, add spec to README

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -6,74 +6,71 @@ const { authorize, ADMIN } = require('../middlewares/auth');
 /**
  * Load product when API with productCode route parameter is hit
  */
-router.param('productCode', controller.load)
+router.param('productCode', controller.load);
 
 router
   .route('/')
   /**
-   * @api {get} api/products List Products
+   * @api {get} api/products | List Products
    */
   .get(controller.list)
-
   /**
-   * @api {post} api/products Create Product
+   * @api {post} api/products | Create Product
    */
   .post(authorize(), controller.create);
 
-  router
+router
   .route('/:productCode')
-
-
    /**
-   * @api {patch} api/products/:productCode Update Product
+   * @api {patch} api/products/:productCode | Update Product
    */
   .patch(authorize(), controller.update)
-
    /**
-   * @api {delete} api/products/productCode    Delete Product
+   * @api {delete} api/products/productCode | Delete Product
    */
   .delete(authorize(), controller.remove);
 
-
-  router
+router
   .route('/details/:productCode')
-
   /**
-   * @api {get} api/products/:productCode Get Product Details
+   * @api {get} api/products/:productCode | Get Product Details
    */
-  .get(authorize(), controller.get)
+  .get(authorize(), controller.get);
 
-  router
+router
   .route('/isProductCodeExists/:productCode')
-
   /**
    * @api {get} api/products/isProductCodeExists/:productCode 
    */
-  .get(authorize(), controller.isProductCodeExists)
-
-  /**
-   * @api {get} api/products/brands Get Product's Brands List
-   * */
-  router
+  .get(authorize(), controller.isProductCodeExists);
+  
+router
   .route('/brands')
-  .get( controller.getBrands)
-
   /**
-   * @api {get} api/products/types Get Product's Types List
-   * */
-  router
+   * @api {get} api/products/brands | Get Product's Brands List
+   */
+  .get(controller.getBrands);
+
+router
   .route('/types')
-  .get( controller.getTypes)
-
   /**
-   * @api {get} api/products/sku Get Product's Types List
-   * */
-  router
-  .route('/sku')
-  .get(controller.getSKUList)
+   * @api {get} api/products/types | Get Product's Types List
+   */
+  .get(controller.getTypes);
 
-  router
+router
+  .route('/sku')
+  /**
+   * @api {get} api/products/sku | Get Product's Types List
+   */
+  .get(controller.getSKUList);
+
+router
   .route('/upload')
+  /**
+   * @api {post} api/products/upload
+   */
   .post(controller.uploadProducts);
+
 
 module.exports = router;
