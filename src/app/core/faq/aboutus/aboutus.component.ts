ab32cd0a566c00037ce970a6a620630289e7f091
alidavid0418 <robbie626827@gmail.com>
2019-05-05 00:39:38 +0900

    accomplish About Us page

--- src/app/core/faq/aboutus/aboutus.component.ts
+++ src/app/core/faq/aboutus/aboutus.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit, Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../../../services/session.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
   styleUrls: ['./aboutus.component.css']
 })
-@Injectable ()
 
 export class AboutusComponent implements OnInit {
 
-  constructor() { }
+  constructor(private session: SessionService,
+    private router: Router
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (!this.session.isLoggedIn()) {
+      this.router.navigate(['/user/login']);
+    }
+  }
 
 }
