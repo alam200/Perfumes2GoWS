7b5f60450ebc7cbefc148e0bb0d9afa29747224f
Ivan Gonzalez <ivandario1998@gmail.com>
2019-09-14 13:49:49 -0500

    Fixing and styling vendors table

--- src/app/core/vendors/vendors-list/vendors-list.component.ts
+++ src/app/core/vendors/vendors-list/vendors-list.component.ts
@@ -25,10 +25,23 @@ interface vendor{
 
 
 export class VendorsListComponent implements OnInit {
+  dtOptions: DataTables.Settings = {};
   vendorToDelete: String;
   vendorID: String;
+  Total_cnt: number = 0;
   vendorsOrig: vendor[];
   vendors: vendor[];
+  defaultOrder = [0, 'asc'];
+  tableColumns = [
+    { data: 'vendorID' },
+    { data: 'SKU' },
+    { data: 'productDescription' },
+    { data: 'purchaseDate' },
+    { data: 'lastPurchasePrice' },
+    { data: 'lastPurchasedQty' },
+    { data: 'delete' }
+  ];
+  columnDefsTarget = [6];
   constructor(private http: HttpClient, 
     private vendorService: VendorsService,
     private spinner: NgxSpinnerService) { }
@@ -61,21 +74,81 @@ export class VendorsListComponent implements OnInit {
           return res;
         });
         this.vendorsOrig = this.vendors;
+        this.Total_cnt = this.vendors.length;
       },
       error => {
         console.log(error);
       }
     )
+    this.dtOptions = {
+      pagingType: 'simple_numbers',
+      serverSide: true,
+      autoWidth: false,
+      processing: false,
+      lengthMenu: [50, 100, 150, 200],
+      order: this.defaultOrder,
+      ajax: (dataTablesParameters: any, callback) => {
+        if (this.vendors) {
+          const orderId = dataTablesParameters.order[0].column;
+          const direction = dataTablesParameters.order[0].dir;
+          const orderProperty = dataTablesParameters.columns[orderId].data;
+          const searchString = dataTablesParameters.search.value;
+          if (searchString !== "") { 
+            this.vendors = this.vendorsOrig.filter(vendor => {
+              const isVendorID = vendor.vendorID.search(searchString) > -1;
+              const isSKU = vendor.SKU.search(searchString) > -1;
+              const isProductDescription = vendor.productDescription.search(searchString) > -1;
+              return isVendorID || isSKU || isProductDescription;
+            });
+          } else {
+            this.vendors = this.vendorsOrig;
+          }
+          if (direction === "asc") {
+            this.vendors.sort((a, b) => {
+              if (orderId > 3) {
+                return a[orderProperty] - b[orderProperty];
+              }
+              return a[orderProperty].localeCompare(b[orderProperty]);
+            });
+          } else {
+            this.vendors.sort((a, b) => {
+              if (orderId > 3) {
+                return b[orderProperty] - a[orderProperty];
+              }
+              return b[orderProperty].localeCompare(a[orderProperty]);
+            });
+          }         
+          this.Total_cnt = this.vendors.length;
+        }
+      },
+      columns: this.tableColumns,
+      columnDefs: [
+        {
+          targets: this.columnDefsTarget,
+          searchable: false,
+          orderable: false,
+          visible: true
+        },
+        { width: '15%', targets: 1 },
+        { width: '10%', targets: 3 },
+        { width: '30%', targets: 4 },
+        { width: '7%', targets: 5 },
+        { width: '8%', targets: 6 }
+      ],
+      language: {
+        emptyTable: 'No data available in table',
+        loadingRecords: 'Please wait...',
+        zeroRecords: 'No matching records found',
+        lengthMenu: "Show _MENU_ entries of: "
+      },
+      drawCallback: (settings) => {
+        $('#datTable_length').parent().removeClass('col-md-6').addClass('col-md-4');
+        $('#datTable_filter').parent().removeClass('col-md-6').addClass('col-md-3');
+        $('#datTable_filter').css('text-align', 'right');
+      }
+    };
     
     this.spinner.hide();
-  }
-
-  Search(){
-    if(this.vendors){
-      this.vendors = this.vendorsOrig.filter(res =>{
-        return res.vendorID.toLocaleLowerCase().match(this.vendorID.toLocaleLowerCase());
-      });
-    }
   }
 
   public closeModal() {
